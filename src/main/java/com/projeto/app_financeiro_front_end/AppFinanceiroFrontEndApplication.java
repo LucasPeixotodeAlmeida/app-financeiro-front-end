75dcfe5200a9e1a3e1244093f99571e15b7b1355
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-04 20:02:38 -0300

    criação front end app-financeiro

---
+++ src/main/java/com/projeto/app_financeiro_front_end/AppFinanceiroFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.app_financeiro_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFinanceiroFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFinanceiroFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/app_financeiro_front_end/AppFinanceiroFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.app_financeiro_front_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinanceiroFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
