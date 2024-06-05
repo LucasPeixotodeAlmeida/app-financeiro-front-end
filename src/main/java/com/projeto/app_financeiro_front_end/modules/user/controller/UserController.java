f7fbf8f5459249b37742307c2d9b56def9418c33
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-06-05 19:14:47 -0300

    criação mensagem de usuario incorreto

--- src/main/java/com/projeto/app_financeiro_front_end/modules/user/controller/UserController.java
+++ src/main/java/com/projeto/app_financeiro_front_end/modules/user/controller/UserController.java
@@ -2,7 +2,9 @@ package com.projeto.app_financeiro_front_end.modules.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/user")
@@ -10,5 +12,14 @@ public class UserController {
     @GetMapping("/login")
     public String Login(){
         return "user/login";
+    }
+
+    @PostMapping("/signIn")
+    public String signIn(RedirectAttributes redirectAttributes, String username, String password){
+        if(username.equals("lucas")){
+            return "user/profile";
+        }
+        redirectAttributes.addFlashAttribute("error_message", "Usuário ou senha incorretos!");
+        return "redirect:/user/login";
     }
 }
